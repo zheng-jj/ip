3b095aa7b6027828f531b64e5813d00965d0975a
zheng-jj <zhengjiongjie12345@gmail.com>
2025-08-20 23:54:42 +0800

    Level-1 Complete

--- src/main/java/Pingu.java
+++ src/main/java/Pingu.java
@@ -1,23 +1,35 @@
+import java.util.Scanner;
 public class Pingu {
     public static void printMsg(String msg, String divider) {
         System.out.println(msg);
         System.out.println(divider);
     }
+
+    public static String getInput(Scanner scanner, String divider) {
+        String input = scanner.nextLine();
+        System.out.println(divider);
+        return input;
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
         String chatbotName = "Pingu";
         String divider = "_".repeat(60);
         String introMessage = "Hello! I'm " + chatbotName + "\n"
                 + "What can I do for you?";
         String closeMessage = "Bye. Hope to see you again soon!";
-        
+        Scanner scanner = new Scanner(System.in);
         // hi bye
         System.out.println(divider);
         printMsg(introMessage, divider);
+        String input = "";
+        String breakCon = "bye";
+        while (!input.equals(breakCon)) {
+            input = getInput(scanner, divider);
+            if (input.equals(breakCon)) {
+                break;
+            }
+            printMsg(input, divider);
+        }
         printMsg(closeMessage, divider);
     }
 }
